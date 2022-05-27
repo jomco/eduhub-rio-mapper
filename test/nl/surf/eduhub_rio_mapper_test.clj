2b891bac4e3c32c81b2de4f686067e364938c2ef
Joost Diepenmaat <joost@zeekat.nl>
2022-05-27 13:36:15 +0200

    Clojure scaffolding erbij

---
+++ test/nl/surf/eduhub_rio_mapper_test.clj
@@ -0,0 +1,7 @@
+(ns nl.surf.eduhub-rio-mapper-test
+  (:require [clojure.test :refer :all]
+            [nl.surf.eduhub-rio-mapper :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
