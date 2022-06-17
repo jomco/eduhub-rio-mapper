e46c1e32e56b3321841d13ee229289f36adf2eb8
Michiel de Mare <mdemare@users.noreply.github.com>
2022-06-17 10:53:13 +0200

    Implemented EducationSpecification except timeline and consumers (#1)
    * Implemented EducationSpecification except timeline and consumers
    
    * Code review improvements
    
    * Code review improvements
    
    * Distinguish between top-level and nested ES
    
    * Removed dissoc-in
    
    * Code review improvements: renamed variables
    
    * Separate namespaces for LanguageTypedString and StudyLoadDescriptor
    
    * Generate enumerables from yaml files in specification. (#2)
    
    * Generate enumerables from yaml files in specification.
    
    * Code review improvements

--- src/nl/surf/eduhub_rio_mapper/ooapi.clj
+++ src/nl/surf/eduhub_rio_mapper/ooapi.clj
@@ -1,12 +1,79 @@
 (ns nl.surf.eduhub-rio-mapper.ooapi
   (:require [clojure.spec.alpha :as s]
-            [nl.surf.eduhub-rio-mapper.ooapi.EducationSpecification :as-alias EducationSpecification]))
+            [nl.surf.eduhub-rio-mapper.ooapi.EducationSpecification :as-alias EducationSpecification]
+            [nl.surf.eduhub-rio-mapper.ooapi.StudyLoadDescriptor :as-alias StudyLoadDescriptor]
+            [nl.surf.eduhub-rio-mapper.ooapi.LanguageTypedString :as-alias LanguageTypedString]
+            [nl.surf.eduhub-rio-mapper.ooapi.enums :as enums]
+            [clojure.string :as string])
+  (:import (java.time.format DateTimeFormatter DateTimeParseException)
+           (java.time LocalDate)
+           (java.util UUID)))
 
-(s/def ::EducationSpecification/educationSpecificationType #{"program" "cluster" "course" "privateProgram"})
-(s/def ::EducationSpecification/validFrom string?)
-(s/def ::EducationSpecification/validTo string?)
+;; Patterns
+(def language-code-pattern #"^[a-z]{2,4}(-[A-Z][a-z]{3})?(-([A-Z]{2}|[0-9]{3}))?$")
+(def date-format (DateTimeFormatter/ofPattern "uuuu-MM-dd"))
+
+;; Validators
+(defn valid-date? [date]
+  (and (string? date)
+       (try (LocalDate/parse date date-format)
+            true
+            (catch DateTimeParseException _ false))))
+
+(defn valid-uuid? [uuid]
+  (and (string? uuid)
+       (try (UUID/fromString uuid)
+            true
+            (catch IllegalArgumentException _ false))))
+
+
+;; Common types
+
+(s/def ::LanguageTypedString/language
+  (s/and string?
+         #(re-matches language-code-pattern %)))
+
+(s/def ::LanguageTypedString/value string?)
+
+(s/def ::EducationSpecification/languageTypedStrings
+  (s/coll-of
+    (s/keys :req-un [::LanguageTypedString/language
+                     ::LanguageTypedString/value])))
+
+(defn valid-codeType?
+  "codeType should be in a predefined set or start with x-"
+  [codeType]
+  (or (contains? enums/codeTypes codeType)
+      (string/starts-with? codeType "x-")))
+
+(s/def ::EducationSpecification/codeType valid-codeType?)
+(s/def ::EducationSpecification/code string?)
+
+(s/def ::EducationSpecification/codeTuple
+  (s/keys :req-un [::EducationSpecification/codeType ::EducationSpecification/code]))
+
+;; Top level response keys
+(s/def ::EducationSpecification/abbreviation (s/and string? #(< (count %) 256)))
+(s/def ::EducationSpecification/description ::EducationSpecification/languageTypedStrings)
 (s/def ::EducationSpecification/educationSpecificationId string?)
+(s/def ::EducationSpecification/educationSpecification valid-uuid?)
 (s/def ::EducationSpecification/educationSpecificationSubType #{"variant"})
+(s/def ::EducationSpecification/educationSpecificationType enums/educationSpecificationTypes)
+(s/def ::EducationSpecification/fieldsOfStudy (s/and string? #(re-matches #"\d{1,4}" %)))
+(s/def ::EducationSpecification/formalDocument enums/formalDocumentTypes)
+(s/def ::EducationSpecification/learningOutcomes (s/coll-of ::EducationSpecification/languageTypedStrings))
+(s/def ::EducationSpecification/level enums/levels)
+(s/def ::EducationSpecification/levelOfQualification #{"1" "2" "3" "4" "4+" "5" "6" "7" "8"})
+(s/def ::EducationSpecification/name ::EducationSpecification/languageTypedStrings)
+(s/def ::EducationSpecification/link (s/and string? #(< (count %) 2048)))
+(s/def ::EducationSpecification/otherCodes (s/coll-of ::EducationSpecification/codeTuple))
+(s/def ::EducationSpecification/primaryCode ::EducationSpecification/codeTuple)
+(s/def ::EducationSpecification/sector enums/sectors)
+(s/def ::StudyLoadDescriptor/value number?)
+(s/def ::StudyLoadDescriptor/studyLoadUnit enums/studyLoadUnits)
+(s/def ::EducationSpecification/studyLoad (s/keys :req-un [::StudyLoadDescriptor/studyLoadUnit ::StudyLoadDescriptor/value]))
+(s/def ::EducationSpecification/validFrom valid-date?)
+(s/def ::EducationSpecification/validTo valid-date?)
 
 (defn valid-type-and-subtype?
   "EducationSpecification should only have subType if type is 'program'."
@@ -19,9 +86,27 @@
 
 (s/def ::EducationSpecification
   (s/and (s/keys :req-un [::EducationSpecification/educationSpecificationType
+                          ::EducationSpecification/name
                           ;; is required because RIO requires `beginDatum`
-                          ::EducationSpecification/validFrom
-                          ::EducationSpecification/educationSpecificationId]
-                 :opt-un [::EducationSpecification/validTo
+                          ::EducationSpecification/educationSpecificationId
+                          ::EducationSpecification/primaryCode
+                          ]
+                 :opt-un [::EducationSpecification/abbreviation
+                          ::EducationSpecification/description
+                          ::EducationSpecification/educationSpecification
+                          ::EducationSpecification/fieldsOfStudy
+                          ::EducationSpecification/formalDocument
+                          ::EducationSpecification/learningOutcomes
+                          ::EducationSpecification/level
+                          ::EducationSpecification/levelOfQualification
+                          ::EducationSpecification/link
+                          ::EducationSpecification/otherCodes
+                          ::EducationSpecification/sector
+                          ::EducationSpecification/studyLoad
                           ::EducationSpecification/educationSpecificationSubType])
          valid-type-and-subtype?))
+
+(s/def ::EducationSpecificationTopLevel
+  (s/and ::EducationSpecification
+         (s/keys :req-un [::EducationSpecification/validFrom]
+                 :opt-un [::EducationSpecification/validTo])))
--- test/nl/surf/eduhub_rio_mapper_test.clj
+++ test/nl/surf/eduhub_rio_mapper_test.clj
@@ -13,8 +13,54 @@
                                  slurp
                                  (json/read-str :key-fn keyword)))
 
-(deftest validate-fixtures
-  (is (s/valid? ::ooapi/EducationSpecification education-specification)))
+;; prints the explanation if the default fixtures aren't valid
+(deftest validate-fixtures-explain
+  (let [problems (get-in (s/explain-data ::ooapi/EducationSpecification education-specification) [:clojure.spec.alpha/problems])]
+    (is (contains? #{nil []} problems))))
+
+(deftest validate-fixtures-name-required
+  (let [value (dissoc education-specification :name)]
+    (is (not (s/valid? ::ooapi/EducationSpecification value)))))
+
+(deftest validate-fixtures-language-required-in-description
+  (let [value (update-in education-specification [:description 0] dissoc :language)]
+    (is (not (s/valid? ::ooapi/EducationSpecification value)))))
+
+(deftest validate-fixtures-invalid-codetype
+  (let [value (assoc-in education-specification [:primaryCode :codeType] "undefined")]
+    (is (not (s/valid? ::ooapi/EducationSpecification value)))))
+
+(deftest validate-fixtures-custom-codetype
+  (is (s/valid? ::ooapi/EducationSpecification (assoc-in education-specification [:primaryCode :codeType] "x-undefined"))))
+
+(deftest validate-invalid-value-in-top-level-attribute
+  (doseq [[key invalid-codes] [[:fieldsOfStudy ["12345" "123a"]]
+                               [:formalDocument ["medal"]]
+                               [:level ["grandmaster"]]
+                               [:sector ["culturele"]]
+                               [:validFrom ["2022-31-12" "29-02-2020"]]
+                               [:validTo ["2022-31-12" "29-02-2020"]]
+                               [:educationSpecification ["123e4567-e89b-12d3-a456" "g23e4567-e89b-12d3-a456-426614174111"]]]]
+    (doseq [invalid-code invalid-codes]
+      (is (not (s/valid? ::ooapi/EducationSpecificationTopLevel (assoc-in education-specification [key] invalid-code)))))))
+
+(deftest validate-fixtures-invalid-otherCodes-codetype
+  (let [value (assoc-in education-specification [:otherCodes 0 :codeType] "undefined")]
+    (is (not (s/valid? ::ooapi/EducationSpecification value)))))
+
+(deftest validate-illegal-language-code-in-all-language-types-string-arrays
+  (doseq [path [[:name 0 :language]
+                [:description 0 :language]
+                [:learningOutcomes 0 0 :language]]]
+    (doseq [invalid-code [nil "" "-" "vrooom" "!" "e"]]
+      (let [eduspec (assoc-in education-specification path invalid-code)]
+        (is (not (s/valid? ::ooapi/EducationSpecification eduspec)))))))
+
+(deftest validate-maxlength-abbreviation
+  (is (not (s/valid? ::ooapi/EducationSpecification (assoc-in education-specification [:abbreviation] (apply str (repeat 257 "a")))))))
+
+(deftest validate-maxlength-link
+  (is (not (s/valid? ::ooapi/EducationSpecification (assoc-in education-specification [:link] (apply str (repeat 2049 "a")))))))
 
 (deftest translate-EducationSpecification
   (testing "translating fixtures"
