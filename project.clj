e10aaf5679faecfb06ad7fd1ec374684ac705a71
Joost Diepenmaat <joost@zeekat.nl>
2022-06-23 16:28:28 +0200

    Ensure data specs can generate data
    This will allow us to exercise the code and check that we'll always
    generate valid RIO data given from valid input.

--- project.clj
+++ project.clj
@@ -4,15 +4,19 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.json "2.4.0"]]
+                 [org.clojure/data.json "2.4.0"]
+                 [com.velisco/strgen "0.2.4"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[clj-kondo "2022.05.27"]
-                                  [clj-commons/clj-yaml "0.7.108"]]
+                   :dependencies [[clj-kondo "2022.05.31"]
+                                  [clj-commons/clj-yaml "0.7.108"]
+                                  [nl.jomco/proof-specs "0.1.3"]]
                    :plugins      [[lein-ancient "0.7.0"]]
                    :aliases      {"lint"  ["run" "-m" "clj-kondo.main" "--lint" "src"]
                                   ;; Enums are generated from yaml files in the open-education-api/specification github project.
                                   ;; To regenerate, call `lein generate-enums $path-to-open-education-api-specification`
                                   ;; This will regenerate `src/nl/surf/eduhub_rio_mapper/enums.clj`
-                                  "generate-enums" ["run" "-m" "generate-enums.main"]}}}
-
+                                  "generate-enums" ["run" "-m" "generate-enums.main"]
+                                  "proof-specs" ["run" "-m" "nl.jomco.proof-specs"
+                                                 "--include-regexps" "nl.surf.*"
+                                                 "--require-namespaces" "nl.surf.eduhub-rio-mapper.ooapi,nl.surf.eduhub-rio-mapper.rio"]}}}
   :repl-options {:init-ns nl.surf.eduhub-rio-mapper})
--- src/nl/surf/eduhub_rio_mapper/ooapi/course.clj
+++ src/nl/surf/eduhub_rio_mapper/ooapi/course.clj
@@ -1,9 +1,9 @@
 (ns nl.surf.eduhub-rio-mapper.ooapi.course
   (:require [clojure.spec.alpha :as s]
             [nl.surf.eduhub-rio-mapper.ooapi.Course :as-alias Course]
-            [nl.surf.eduhub-rio-mapper.ooapi :as ooapi]))
+            [nl.surf.eduhub-rio-mapper.ooapi.common :as common]))
 
-(s/def ::Course/name ::ooapi/LanguageTypedStrings)
+(s/def ::Course/name ::common/LanguageTypedStrings)
 
 (s/def ::Course
   (s/keys :req-un [::Course/name]
