b01684d4cc785aaea9815284a430a81f2abd32fe
Joost Diepenmaat <joost@zeekat.nl>
2022-07-05 09:29:59 +0200

    Configure logging dependencies.
    Enable logback in dev mode

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.json "2.4.0"]
+  :dependencies [[ch.qos.logback.contrib/logback-jackson "0.1.5"]
+                 [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
+                 [ch.qos.logback/logback-classic "1.2.11"]
                  [com.velisco/strgen "0.2.4"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]
                  [org.clojure/data.xml "0.0.8"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[clj-kondo "2022.05.31"]
-                                  [clj-commons/clj-yaml "0.7.108"]
+                   :dependencies [[clj-commons/clj-yaml "0.7.108"]
+                                  [clj-kondo "2022.06.22"]
                                   [nl.jomco/proof-specs "0.1.4"]]
                    :plugins      [[lein-ancient "0.7.0"]]
                    :aliases      {"lint"  ["run" "-m" "clj-kondo.main" "--lint" "src"]
