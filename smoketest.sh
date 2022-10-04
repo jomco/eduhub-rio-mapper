f7cb0fad9aac24b1b60d18e3b6174f11b65f7aba
Remco van 't Veer <remco@remworks.net>
2022-10-04 16:30:28 +0200

    Include worker and status polling in smoketest

--- smoketest.sh
+++ smoketest.sh
@@ -26,25 +26,57 @@ export API_HOSTNAME=localhost
 
 # run with trampoline because otherwise lein will fork a new process
 # and then we can't kill the server later
-lein trampoline mapper serve-api&
-SERVER_PID=$!
+lein trampoline mapper serve-api &
+API_SERVER_PID=$!
+trap "kill $API_SERVER_PID" EXIT
 
-kill_server(){
-    if [ -n "${SERVER_PID}" ]; then
-	kill $SERVER_PID
+lein trampoline mapper worker &
+WORKER_SERVER_PID=$!
+# Note: traps get overwritten
+trap "kill $API_SERVER_PID $WORKER_SERVER_PID" EXIT
+
+# Give api server some time to startup
+sleep 20
+
+ROOT_URL="http://${API_HOSTNAME}:${API_PORT}"
+
+URL="${ROOT_URL}/job/upsert/education-specifications/${EDUCATION_SPECIFICATION_ID}"
+echo Post upsert
+UPSERT_TOKEN=$(curl -s --fail-with-body -X POST -H "X-Schac-Home: ${SCHAC_HOME}" "$URL" | jq -r .token)
+echo "  token=$UPSERT_TOKEN"
+echo
+
+URL="${ROOT_URL}/job/delete/education-specifications/${EDUCATION_SPECIFICATION_ID}"
+echo Post delete
+DELETE_TOKEN=$(curl -s --fail-with-body -X POST -H "X-Schac-Home: ${SCHAC_HOME}" "$URL" | jq -r .token)
+echo "  token=$DELETE_TOKEN"
+echo
+
+UPSERT_DONE=
+DELETE_DONE=
+
+while [ -z "$UPSERT_DONE" ] || [ -z "$DELETE_DONE" ]; do
+    sleep 2
+
+    if [ -z "$UPSERT_DONE" ]; then
+        URL="$ROOT_URL/status/$UPSERT_TOKEN"
+        echo Status upsert
+        UPSERT_STATE=$(curl -s --fail-with-body "$URL")
+        UPSERT_STATUS="$(echo "$UPSERT_STATE" | jq -r .status)"
+        echo "$UPSERT_STATE" | jq
+        echo
+        [ "$UPSERT_STATUS" = 'done' ] || [ "$UPSERT_STATUS" = 'error' ] \
+            && UPSERT_DONE=t
     fi
-}
 
-trap 'kill_server' EXIT
-
-sleep 10
-
-ROOT_URL="http://${API_HOSTNAME}:${API_PORT}/"
-set -x
-curl --fail-with-body -H "X-Schac-Home: ${SCHAC_HOME}" -X 'POST' "${ROOT_URL}job/upsert/education-specifications/${EDUCATION_SPECIFICATION_ID}"
-
-sleep 5
-
-curl  --fail-with-body -H "X-Schac-Home: ${SCHAC_HOME}" -X 'POST' "${ROOT_URL}job/delete/education-specifications/${EDUCATION_SPECIFICATION_ID}"
-
-sleep 5
+    if [ -z "$DELETE_DONE" ]; then
+        URL="$ROOT_URL/status/$DELETE_TOKEN"
+        echo Status delete
+        DELETE_STATE=$(curl -s --fail-with-body "$URL")
+        DELETE_STATUS="$(echo "$DELETE_STATE" | jq -r .status)"
+        echo "$DELETE_STATE" | jq
+        echo
+        [ "$DELETE_STATUS" = 'done' ] || [ "$DELETE_STATUS" = 'error' ] \
+            && DELETE_DONE=t
+    fi
+done
