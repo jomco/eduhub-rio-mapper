2bdb3a2f014087f28dc717c7709c3749e26d9b6a
Remco van 't Veer <remco@remworks.net>
2022-10-06 16:34:32 +0200

    Avoid failing job from killing worker

--- src/nl/surf/eduhub_rio_mapper/job.clj
+++ src/nl/surf/eduhub_rio_mapper/job.clj
@@ -1,5 +1,6 @@
 (ns nl.surf.eduhub-rio-mapper.job
-  (:require [nl.surf.eduhub-rio-mapper.errors :refer [result->]]
+  (:require [clojure.tools.logging :as log]
+            [nl.surf.eduhub-rio-mapper.errors :refer [result->]]
             [nl.surf.eduhub-rio-mapper.ooapi :as ooapi])
   (:refer-clojure :exclude [run!]))
 
@@ -7,11 +8,15 @@
   "Run given job and return result."
   [{:keys [handle-deleted handle-updated mutate]}
    {:keys [id type action institution-schac-home]}]
-  (let [handler (case action
-                  "delete" handle-deleted
-                  "upsert" handle-updated)
-        job     {::ooapi/id              id
-                 ::ooapi/type            type
-                 :action                 action
-                 :institution-schac-home institution-schac-home}]
-    (result-> (handler job) (mutate))))
+  (let [job {::ooapi/id              id
+             ::ooapi/type            type
+             :action                 action
+             :institution-schac-home institution-schac-home}]
+    (try
+      (result-> ((case action
+                   "delete" handle-deleted
+                   "upsert" handle-updated) job) (mutate))
+      (catch Exception ex
+        (log/error "Job run failed" ex)
+        {:errors {:job       job
+                  :exception ex}}))))
