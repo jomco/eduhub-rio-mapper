78a18bd71d036a35c60fed20eea0386cc7a454ea
Michiel de Mare <michieldemare@gmail.com>
2022-10-28 15:22:42 +0200

    More codereview fixes

--- src/nl/surf/eduhub_rio_mapper/relation_handler.clj
+++ src/nl/surf/eduhub_rio_mapper/relation_handler.clj
@@ -14,15 +14,27 @@
 (s/def ::Relation/relation-set
   (s/and set? (s/coll-of ::Relation/relation)))
 
+(defn- narrow-valid-daterange
+  "The relation's valid-from and valid-to is stricter than that of the parent or child.
+
+  It starts as soon as both entities are valid, and ends as soon as either entity is no longer valid (may be nil)."
+  [{:keys [parent child] :as relation}]
+  (let [valid-from (last (sort (map :validFrom [parent child])))
+        valid-to (first (sort (keep :validTo [parent child])))]
+    (assoc relation
+      :valid-from valid-from
+      :valid-to valid-to)))
+
+(defn- turn-into-relations [{:keys [parent child] :as relation}]
+  (assoc (select-keys relation [:valid-from :valid-to])
+    :parent-opleidingseenheidcode (:rio-code parent)
+    :child-opleidingseenheidcode (:rio-code child)))
+
 (defn- expected-relations [parent children]
-  (map (fn expected-relations-map [child]
-         (let [valid-from (last (sort (keep :validFrom [parent child])))
-               valid-to (first (sort (keep :validTo [parent child])))]
-           {:parent-opleidingseenheidcode (:rio-code parent)
-            :child-opleidingseenheidcode (:rio-code child)
-            :valid-from valid-from
-            :valid-to valid-to}))
-       children))
+  (->> children
+       (map (fn [child] {:parent parent :child child}))
+       (map narrow-valid-daterange)
+       (map turn-into-relations)))
 
 (defn- program-type? [eduspec] (= "program" (:educationSpecificationType eduspec)))
 
@@ -50,17 +62,13 @@
 (defn mutate-relation
   "Returns the request data needed to perform a mutation (either an insertion or a deletion)."
   [mutate-type institution-oin {:keys [parent-opleidingseenheidcode child-opleidingseenheidcode valid-from valid-to]}]
-  (let [rio-sexp (if (and (= mutate-type :insert)
-                          (some? valid-to))
-                   [[:duo:opleidingsrelatie
-                     [:duo:begindatum valid-from]
-                     [:duo:einddatum valid-to]
-                     [:duo:opleidingseenheidcode parent-opleidingseenheidcode]
-                     [:duo:opleidingseenheidcode child-opleidingseenheidcode]]]
-                   [[:duo:opleidingsrelatie
-                     [:duo:begindatum valid-from]
-                     [:duo:opleidingseenheidcode parent-opleidingseenheidcode]
-                     [:duo:opleidingseenheidcode child-opleidingseenheidcode]]])]
+  (let [rio-sexp `[[:duo:opleidingsrelatie
+                   [:duo:begindatum ~valid-from]
+                   ~@(when (and (= mutate-type :insert)
+                                (some? valid-to))
+                       [[:duo:einddatum valid-to]])
+                   [:duo:opleidingseenheidcode ~parent-opleidingseenheidcode]
+                   [:duo:opleidingseenheidcode ~child-opleidingseenheidcode]]]]
     {:action     (case mutate-type :insert "aanleveren_opleidingsrelatie"
                                    :delete "verwijderen_opleidingsrelatie")
      :sender-oin institution-oin
@@ -77,16 +85,10 @@
     (-> (mutate-relation :delete institution-oin rel)
         mutate)))
 
-(defn after-upsert
-  "Calculates which relations exist in ooapi, which relations exist in RIO, and synchronizes them.
-
-  Only relations between education-specifications are considered; specifically, relations with type program,
-  one with no subtype and one with subtype variant.
-  To perform synchronization, relations are added and deleted in RIO."
-  [eduspec institution-oin institution-schac-home {:keys [getter resolver ooapi-loader mutate]}]
-  {:pre [eduspec institution-schac-home]}
+(defn- relation-mutations
+  [eduspec {:keys [institution-oin institution-schac-home] :as _job} {:keys [getter resolver ooapi-loader]}]
   (let [rio-code (fn [entity]
-                  (-> entity :educationSpecificationId (resolver institution-oin) :code))
+                   (-> entity :educationSpecificationId (resolver institution-oin) :code))
         load-eduspec (fn [id]
                        (let [es (ooapi-loader {::ooapi/type            "education-specification"
                                                ::ooapi/id              id
@@ -95,10 +97,24 @@
                            (assoc es :rio-code code))))
         actual (load-relation-data (rio-code eduspec) getter institution-oin)]
     (when-let [[rel-dir entity] (case (subtype eduspec)
-                                "variant" [:child (load-eduspec (:parent eduspec))]
-                                nil       [:parent (->> (keep load-eduspec (:children eduspec))
-                                                        (filter valid-child?))])]
-      (let [{:keys [missing superfluous]} (relation-differences eduspec rel-dir entity actual)
-            mutator (fn [rel op] (-> (mutate-relation op institution-oin rel) (mutate)))]
-        (doseq [rel missing]     (mutator rel :insert))
-        (doseq [rel superfluous] (mutator rel :delete))))))
+                                  "variant" [:child (load-eduspec (:parent eduspec))]
+                                  nil       [:parent (->> (keep load-eduspec (:children eduspec))
+                                                          (filter valid-child?))])]
+      (relation-differences eduspec rel-dir entity actual))))
+
+(defn- mutate-relations!
+  [{:keys [missing superfluous]} mutate institution-oin]
+  (let [mutator (fn [rel op] (-> (mutate-relation op institution-oin rel) (mutate)))]
+    (doseq [rel missing]     (mutator rel :insert))
+    (doseq [rel superfluous] (mutator rel :delete))))
+
+(defn after-upsert
+  "Calculates which relations exist in ooapi, which relations exist in RIO, and synchronizes them.
+
+  Only relations between education-specifications are considered; specifically, relations with type program,
+  one with no subtype and one with subtype variant.
+  To perform synchronization, relations are added and deleted in RIO."
+  [eduspec job {:keys [mutate] :as handlers}]
+  {:pre [eduspec (:institution-schac-home job)]}
+  (-> (relation-mutations eduspec job handlers)
+      (mutate-relations! mutate (:institution-oin job))))
