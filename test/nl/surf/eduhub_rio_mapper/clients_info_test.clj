15da6e19f889291669b591a7c406abaa1786198d
Joost Diepenmaat <joost@zeekat.nl>
2022-11-02 12:40:05 +0100

    Test course in smoketest
    This change tries to add and then remove a course related to the
    already upserted education-specification.

--- test/nl/surf/eduhub_rio_mapper/clients_info_test.clj
+++ test/nl/surf/eduhub_rio_mapper/clients_info_test.clj
@@ -4,7 +4,7 @@
 
 (deftest test-make-clients-info
   (let [info (clients-info/read-clients-data {:path "test/test-clients.json"})]
-    (is (= {:client-id              "rio-mapper-dev.jomco.nl"
+    (is (= {:client-id              "rio-mapper-dev6.jomco.nl"
             :institution-schac-home "demo06.test.surfeduhub.nl"
             :institution-oin        "0000000700025BE00000"}
-           (clients-info/client-info info "rio-mapper-dev.jomco.nl")))))
+           (clients-info/client-info info "rio-mapper-dev6.jomco.nl")))))
