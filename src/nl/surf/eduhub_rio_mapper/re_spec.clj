bc1a483bfb0bf974c52f694bb2701d668b5d1d23
Joost Diepenmaat <joost@zeekat.nl>
2022-11-03 11:03:19 +0100

    Report slightly nicer error on text length specs

--- src/nl/surf/eduhub_rio_mapper/re_spec.clj
+++ src/nl/surf/eduhub_rio_mapper/re_spec.clj
@@ -17,13 +17,20 @@
   [s]
   (re-find #"(<(\S|\Z))|(&\S+;)" s))
 
-(defn text-spec
+(defn length-between?
+  [min-length max-length s]
+  (<= min-length (count s) max-length))
+
+(defmacro text-spec
   "Define a string spec with a minimum and maximum length.
 
   Also ensures that the string does not contain any text sequences
   that are considered invalid by the RIO API (meaning, we disallow
-  anything that looks like HTML tags or escape codes)"
+  anything that looks like HTML tags or escape codes)
+
+  This is a macro and not a function so that min-length and max-length
+  will be reported in spec errors."
   [min-length max-length]
-  (s/spec (s/and string?
-                 #(<= min-length (count %) max-length)
-                 #(not (looks-like-html? %)))))
+  `(s/spec (s/and string?
+                  #(length-between? ~min-length ~max-length %)
+                  #(not (looks-like-html? %)))))
