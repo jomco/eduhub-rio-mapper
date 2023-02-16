e783eebc92d6befbcadcd7d8016f19b5f55bcd98
Michiel de Mare <michieldemare@gmail.com>
2023-02-16 17:19:47 +0100

    Added spec for educationOffererCode (#154)

--- src/nl/surf/eduhub_rio_mapper/ooapi/common.clj
+++ src/nl/surf/eduhub_rio_mapper/ooapi/common.clj
@@ -74,6 +74,9 @@
 
 ;; Common types
 
+(s/def ::educationOffererCode
+  (re-spec #"\d\d\dA\d\d\d"))
+
 (s/def ::LanguageTypedString/language
   (re-spec #"^[a-z]{2,4}(-[A-Z][a-z]{3})?(-([A-Z]{2}|[0-9]{3}))?$"))
 
