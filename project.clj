e394c71cb596177a73700e48de4a5e358d72b161
Michiel de Mare <michieldemare@gmail.com>
2023-09-13 11:01:32 +0200

    Upgraded dependencies

--- project.clj
+++ project.clj
@@ -20,13 +20,13 @@
                    org.clojure/clojurescript]]
                  [commons-io "2.13.0"]
                  [compojure "1.7.0"]
-                 [info.sunng/ring-jetty9-adapter "0.22.1"]
+                 [info.sunng/ring-jetty9-adapter "0.30.0"]
                  [nl.jomco/envopts "0.0.4"]
                  [nl.jomco/ring-trace-context "0.0.8"]
                  [nl.jomco/clj-http-status-codes "0.1"]
                  [org.apache.santuario/xmlsec "3.0.2" :exclusions [org.slf4j/slf4j-api]] ;; we use dependency from ring-jetty9-adapter
                  [org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.async "1.6.681" :exclusions [org.ow2.asm/asm]] ;; we use dependency from ring-jetty9-adapter
+                 [org.clojure/core.async "1.6.681"] ;; we use dependency from ring-jetty9-adapter
                  [org.clojure/core.memoize "1.0.257"]
                  [org.clojure/data.json "2.4.0"]
                  [org.clojure/data.xml "0.0.8"]
@@ -34,7 +34,7 @@
                  [org.clojure/tools.logging "1.2.4"]
                  [org.clojure/tools.reader "1.3.6"]
                  [ring/ring-core "1.10.0"]
-                 [ring/ring-defaults "0.3.4"]
+                 [ring/ring-defaults "0.4.0"]
                  [ring/ring-json "0.5.1"]]
 
   :java-source-paths ["src"]
