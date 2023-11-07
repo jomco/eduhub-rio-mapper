35a6e398212557eb10062d1240fee317a37840f3
Remco van 't Veer <remco@remworks.net>
2023-11-07 15:26:21 +0100

    Use ring-jetty-adapter proper
    The ring-jetty9-adapter library requires too many exclusions breaking
    new dependencies.

--- project.clj
+++ project.clj
@@ -5,26 +5,40 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[ch.qos.logback.contrib/logback-jackson "0.1.5"]
                  [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
-                 [ch.qos.logback/logback-classic "1.4.11" :exclusions [org.slf4j/slf4j-api]] ;; we use dependency from ring-jetty9-adapter
-                 [cheshire "5.12.0"]
+                 [ch.qos.logback/logback-classic "1.4.11"]
+
                  [clj-http "3.12.3"]
                  [clj-time "0.15.2"]
-                 ;; CVE-2020-28491 (ring-json -> cheshire)
-                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.15.3" :upgrade :keep-this-version]
-                 [com.fasterxml.jackson.core/jackson-core "2.15.3"]
-                 [com.taoensso/carmine "3.3.2" :exclusions [commons-codec]] ;; because [clj-http "3.12.3"] uses [commons-codec "1.15"]
-                 [com.velisco/strgen "0.2.4"
-                  :exclusions
-                  ;; unused in this project
-                  [criterium
-                   org.clojure/clojurescript]]
                  [commons-io "2.15.0"]
+
+                 [cheshire "5.12.0"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.15.3"
+                  ;; CVE-2020-28491 (ring-json -> cheshire)
+                  :upgrade :keep-this-version]
+                 [com.fasterxml.jackson.core/jackson-core "2.15.3"]
+
+                 [com.taoensso/carmine "3.3.2"
+                  ;; because [clj-http "3.12.3"] uses [commons-codec "1.15"]
+                  :exclusions [commons-codec]]
+                 [com.velisco/strgen "0.2.4"
+                  ;; unused in this project
+                  :exclusions [criterium
+                               org.clojure/clojurescript]]
+
                  [compojure "1.7.0"]
-                 [info.sunng/ring-jetty9-adapter "0.22.3" :upgrade :keep-this-version :exclusions [org.ow2.asm/asm]]
+                 [ring/ring-jetty-adapter "1.10.0"
+                   ;; included below
+                  :exclusions [org.eclipse.jetty/jetty-server]]
+                 [org.eclipse.jetty/jetty-server "9.4.53.v20231009"
+                   ;; CVE-2023-40167, CVE-2023-44487
+                  :upgrade :keep-this-version]
+
                  [nl.jomco/envopts "0.0.4"]
                  [nl.jomco/ring-trace-context "0.0.8"]
                  [nl.jomco/clj-http-status-codes "0.1"]
-                 [org.apache.santuario/xmlsec "4.0.0" :exclusions [org.slf4j/slf4j-api]] ;; we use dependency from ring-jetty9-adapter
+
+                 [org.apache.santuario/xmlsec "4.0.0"]
+
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/core.async "1.6.681"]
                  [org.clojure/core.memoize "1.0.257"]
@@ -33,6 +47,7 @@
                  [org.clojure/tools.cli "1.0.219"]
                  [org.clojure/tools.logging "1.2.4"]
                  [org.clojure/tools.reader "1.3.6"]
+
                  [ring/ring-core "1.10.0"]
                  [ring/ring-defaults "0.3.4" :upgrade :keep-this-version]
                  [ring/ring-json "0.5.1"]]
