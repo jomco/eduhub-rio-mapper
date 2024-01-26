f6ec65d5bb9345616a4972f3224f3a4a18700ec1
Michiel de Mare <michieldemare@gmail.com>
2024-01-26 11:22:05 +0100

    Move vcr code to test helper (#273)
    * Move vcr code to test helper
    
    * Make public

--- test/nl/surf/eduhub_rio_mapper/test_helper.clj
+++ test/nl/surf/eduhub_rio_mapper/test_helper.clj
@@ -19,8 +19,13 @@
 (ns nl.surf.eduhub-rio-mapper.test-helper
   (:require
     [clojure.data.json :as json]
+    [clojure.edn :as edn]
     [clojure.java.io :as io]
-    [clojure.test :refer :all]))
+    [clojure.pprint :refer [pprint]]
+    [clojure.string :as str]
+    [clojure.test :refer :all])
+  (:import
+    [java.io PushbackReader]))
 
 (defn load-json [path]
   (some-> path
@@ -37,3 +42,59 @@
 (defn wait-for-expected [expected val-atom max-sec]
   (wait-while-predicate #(not= % expected) val-atom max-sec)
   (is (= expected @val-atom)))
+
+(defn- ls [dir-name]
+  (map #(.getName %) (.listFiles (io/file dir-name))))
+
+(defn only-one-if-any [list]
+  (assert (< (count list) 2) (prn-str list))
+  (first list))
+
+(defn- numbered-file [basedir nr]
+  {:post [(some? %)]}
+  (let [filename (->> basedir
+                      (ls)
+                      (filter #(.startsWith % (str nr "-")))
+                      (only-one-if-any))]
+    (when-not filename (throw (ex-info (format "No recorded request found for dir %s nr %d" basedir nr) {})))
+    (str basedir "/" filename)))
+
+(defn req-name [request]
+  (let [action (get-in request [:headers "SOAPAction"])]
+    (if action
+      (last (str/split action #"/"))
+      (-> request :url
+          (subs (count "https://gateway.test.surfeduhub.nl/"))
+          (str/replace \/ \-)
+          (str/split #"\?")
+          first))))
+
+(defn make-playbacker [root idx _]
+  (let [count-atom (atom 0)
+        dir        (numbered-file root idx)]
+    (fn [_ actual-request]
+      (let [i                (swap! count-atom inc)
+            fname            (numbered-file dir i)
+            recording        (with-open [r (io/reader fname)] (edn/read (PushbackReader. r)))
+            recorded-request (:request recording)]
+        (doseq [property-path [[:url] [:method] [:headers "SOAPAction"]]]
+          (let [expected (get-in recorded-request property-path)
+                actual   (get-in actual-request property-path)]
+            (is (= expected actual)
+                (str "Unexpected property " (last property-path)))))
+        (:response recording)))))
+
+(defn make-recorder [root idx desc]
+  (let [mycounter (atom 0)]
+    (fn [handler request]
+      (let [response  (handler request)
+            counter   (swap! mycounter inc)
+            file-name (str root "/" idx "-" desc "/" counter "-" (req-name request) ".edn")
+            headers   (select-keys (:headers request) ["SOAPAction" "X-Route"])]
+        (io/make-parents file-name)
+        (with-open [w (io/writer file-name)]
+          (pprint {:request  (assoc (select-keys request [:method :url :body])
+                               :headers headers)
+                   :response (select-keys response [:status :body])}
+                  w))
+        response))))
