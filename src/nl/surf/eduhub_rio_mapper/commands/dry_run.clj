d87b28ca9ea07ac3a0ba421b229cdbdcc0cde2ca
Michiel de Mare <michieldemare@gmail.com>
2024-04-17 12:06:40 +0200

    Added copyright

--- src/nl/surf/eduhub_rio_mapper/commands/dry_run.clj
+++ src/nl/surf/eduhub_rio_mapper/commands/dry_run.clj
@@ -1,3 +1,21 @@
+;; This file is part of eduhub-rio-mapper
+;;
+;; Copyright (C) 2022 SURFnet B.V.
+;;
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Affero General Public License
+;; as published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Affero General Public License for more details.
+;;
+;; You should have received a copy of the GNU Affero General Public
+;; License along with this program.  If not, see
+;; <https://www.gnu.org/licenses/>.
+
 (ns nl.surf.eduhub-rio-mapper.commands.dry-run
   (:require [nl.surf.eduhub-rio-mapper.ooapi.common :as common]
             [nl.surf.eduhub-rio-mapper.rio.aangeboden-opleiding :as aangeboden-opleiding]
