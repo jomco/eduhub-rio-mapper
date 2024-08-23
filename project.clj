5439748fd657a097c35fcc967bb62d247b0d5b11
Michiel de Mare <michieldemare@gmail.com>
2024-08-23 13:23:57 +0200

    Upgraded deps (#326)

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jomco/eduhub-rio-mapper"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
                  [org.clojure/core.async "1.6.681"]
                  [org.clojure/core.memoize "1.1.266"]
                  [com.velisco/strgen "0.2.5" :exclusions [org.clojure/clojurescript]]
@@ -36,7 +36,7 @@
                  [org.clojure/tools.logging "1.3.0"]
                  [ch.qos.logback.contrib/logback-jackson "0.1.5"]
                  [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
-                 [ch.qos.logback/logback-classic "1.5.6"]
+                 [ch.qos.logback/logback-classic "1.5.7"]
                  [com.fasterxml.jackson.core/jackson-core "2.17.2"]
                  [com.fasterxml.jackson.core/jackson-databind "2.17.2"]
 
