7561380c08a9123b49eb22fb058bf0da47ea4799
Michiel de Mare <michieldemare@gmail.com>
2024-12-18 13:23:08 +0100

    Upgraded deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [org.clojure/core.async "1.6.681"]
+                 [org.clojure/core.async "1.7.701"]
                  [org.clojure/core.memoize "1.1.266"]
                  [com.velisco/strgen "0.2.5" :exclusions [org.clojure/clojurescript]]
 
